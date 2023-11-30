31005651cf3fc94519303c5a4ff7943e05503be4
powerofcode2023 <holbertonc21@gmail.com>
2023-11-30 20:17:14 +0100

    fixed code

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""This module defines a class to manage DB storage for hbnb clone"""
 from models.base_model import Base
 from models.user import User
 from models.place import Place
@@ -10,6 +10,7 @@ from models.review import Review
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
+
 MYSQL_USER = getenv('HBNB_MYSQL_USER')
 MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
 MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
@@ -17,25 +18,23 @@ MYSQL_DB = getenv('HBNB_MYSQL_DB')
 
 
 class DBStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """This class manages storage of hbnb models in a MySQL database"""
     __engine = None
     __session = None
 
     def __init__(self):
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(MYSQL_USER,
-                                              MYSQL_PWD,
-                                              MYSQL_HOST,
-                                              MYSQL_DB))
-        hbnb_env = getenv("HBNB_ENV")
-        if hbnb_env == "test":
+        """Instantiate a DBStorage object"""
+        self.__engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}?charset=utf8mb4'.format(
+                MYSQL_USER, MYSQL_PWD, MYSQL_HOST, MYSQL_DB),
+            pool_pre_ping=True)
+        if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """variable"""
+        """Query all objects depending on the class name (cls)"""
         dic = {}
         if cls:
-            """conditional"""
             if type(cls) is str:
                 cls = eval(cls)
             query = self.__session.query(cls)
@@ -43,35 +42,35 @@ class DBStorage:
                 key = "{}.{}".format(type(elem).__name__, elem.id)
                 dic[key] = elem
         else:
-            lista = [State, City, User, Place, Review, Amenity]
-            for clase in lista:
+            classes = [State, City, User, Place, Review, Amenity]
+            for clase in classes:
                 query = self.__session.query(clase)
                 for elem in query:
                     key = "{}.{}".format(type(elem).__name__, elem.id)
                     dic[key] = elem
-        """return"""
-        return (dic)
+        return dic
 
     def new(self, obj):
-        """new"""
+        """Add the object to the current database session"""
         self.__session.add(obj)
 
     def save(self):
-        """save"""
+        """Commit all changes of the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete"""
+        """Delete obj from the current database session if not None"""
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        """"""
+        """Reloads data from the database"""
         Base.metadata.create_all(self.__engine)
-        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(sec)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
         self.__session = Session()
 
     def close(self):
-        """close"""
+        """Close the current SQLAlchemy session"""
         self.__session.close()
