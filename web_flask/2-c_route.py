3d65a51e8b8f7e342df732c381d2248591232133
powerofcode2023 <holbertonc21@gmail.com>
2023-12-12 14:41:30 +0100

    added comments

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,16 +5,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """ Function that returns 'Hello HBNB!' """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ Returns 'HBNB' """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """ Return 'C ' followed by the text """
     return 'C ' + text.replace('_', ' ')
 
 
