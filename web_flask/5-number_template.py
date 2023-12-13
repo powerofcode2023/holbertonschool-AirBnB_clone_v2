02fe9e417aa4b400eacab4429b8ec403ec470d69
powerofcode2023 <holbertonc21@gmail.com>
2023-12-13 10:08:13 +0100

    fixed pycodestyle

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,11 +35,13 @@ def number(n):
     """ Returns '<n> is a number' if n is an integer """
     return '{} is a number'.format(n)
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """ Display a HTML page only if n is an integer """
     path = '5-number.html'
     return render_template('5-number.html', n=n)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
