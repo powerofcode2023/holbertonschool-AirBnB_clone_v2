10266b1cf519bce7514cae81134c99c4e6f149a7
abdel <holbertonc21@gmail.com>
2023-12-15 14:39:33 +0100

    Write a script that starts a Flask web application

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module for starting a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Displays a HTML page with a list of all State objects"""
+    states = storage.all("State").values()    
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
